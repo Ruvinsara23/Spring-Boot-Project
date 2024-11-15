00a93d3a2e749f14ad4cea612cd58cab68eaed6a
Pansilu Ruvinsara <ruvinsara23@gmail.com>
2024-11-15 21:51:42 +0530

    continue

--- demo/src/main/java/com/example/demo/model/User.java
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -1,4 +1,17 @@
 package com.example.demo.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+// creating entitiy
+@Entity //
+@NoArgsConstructor
+@AllArgsConstructor// for create table
 public class User {
+    @Id
+private int id;
+private String name;
 }
--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -1,4 +1,27 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.UserDTO;
+import com.example.demo.model.User;
+import com.example.demo.repo.UserRepo;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.internal.bytebuddy.description.method.MethodDescription;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional // data transaction
 public class UserService {
+    @Autowired
+    private UserRepo userRepo;
+    // data will be filtered through the repo
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<UserDTO> getAllUser(){
+      List<User>userList=userRepo.findAll();
+        return modelMapper.map(userList new MethodDescription.TypeToken());
+    }
 }
+//flow =  Controller->DTO->Model->user repo->service(data will be stored as efferent format
